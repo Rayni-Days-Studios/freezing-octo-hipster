98979b4b165a7ec3dd79aa2ed5caa6af3e9cb2a7
Rasmus Larsen <rasmuslarsen@raynidadys.com>
2014-10-24 21:44:31 +0200

    Add Engine solution and Vector class

---
+++ Engine/Math/Vector3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Engine
+{
+    class Vector3
+    {
+        public Vector3(float  x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public float Z { get; set; }
+
+        public float Magnitude()
+        {
+            return (float) Math.Sqrt(X*X + Y*Y + Z*Z);
+        }
+
+        public Vector3 Normalize()
+        {
+            return new Vector3(X/Magnitude(), Y/Magnitude(), Z/Magnitude());
+        }
+
+        public static Vector3 operator +(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        public override string ToString()
+        {
+            return "(" + X + ", " + Y + ", " + Z + ")";
+        }
+    }
+}
---
+++ Engine/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vector = new Vector3(4, 5, 7);
+            Console.Write("{0}\n", vector);
+        }
+    }
+}
