9328d52982be5017e77a385d8f9fae0252a6854a
Rasmus Larsen <rasmuslarsen@raynidadys.com>
2014-10-26 15:35:22 +0100

    Remove some components, so engine design can be talked over

--- Engine/Program.cs
+++ Engine/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using Engine.Components;
-using Engine.GameObject;
-
-namespace Engine
+﻿namespace Engine
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var obj = new GameObject.GameObject(new Transform(new Vector3(1, 2, 3), new Vector3(0, 0, 0), new Vector3(1, 1, 1)));
-            Console.Write(obj.GetComponent());
+
         }
     }
 }
