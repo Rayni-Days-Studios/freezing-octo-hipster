a054fb146f8599113022f4fa4e21e61afcfee8b8
unknown <jens5176@VID45NFH12.VID.net.local>
2014-10-28 10:15:49 +0100

    Adds OpenTK example code

--- Engine/Program.cs
+++ Engine/Program.cs
@@ -1,15 +1,65 @@
 ﻿using System;
 using Engine.Math;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
 
 namespace Engine
 {
     class Program
     {
+        [STAThread]
         static void Main()
         {
-            var vector = Vector2.Down;
+            using (var game = new GameWindow())
+            {
+                game.Load += (sender, e) =>
+                {
+                    // setup settings, load textures, sounds
+                    game.VSync = VSyncMode.On;
+                };
 
-            Console.Write(vector + "\n");
+                game.Resize += (sender, e) =>
+                {
+                    GL.Viewport(0, 0, game.Width, game.Height);
+                };
+
+                game.UpdateFrame += (sender, e) =>
+                {
+                    if (game.Keyboard[Key.Escape])
+                    {
+                        game.Exit();
+                    }
+                };
+
+                game.RenderFrame += (sender, e) =>
+                {
+                    // render graphics
+                    GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+                    GL.MatrixMode(MatrixMode.Projection);
+                    GL.LoadIdentity();
+                    GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+
+                    GL.Begin(PrimitiveType.Triangles);
+
+                    GL.Color3(Color.MidnightBlue);
+                    GL.Vertex2(-1.0f, 1.0f);
+                    GL.Color3(Color.SpringGreen);
+                    GL.Vertex2(0.0f, -1.0f);
+                    GL.Color3(Color.Ivory);
+                    GL.Vertex2(1.0f, 1.0f);
+
+                    GL.End();
+
+                    game.SwapBuffers();
+                };
+
+                // Run the game at 60 updates per second
+                game.Run(60.0);
+            }
         }
     }
 }
